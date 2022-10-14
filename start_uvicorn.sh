cb40ed2287b6c0cb57ccb972d186b642261511e7
Agustin Rivera <arivera@arivera-mlt.client.nvidia.com>
2022-10-13 17:19:43 -0700

    Configuration scripts

--- start_uvicorn.sh
+++ start_uvicorn.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
+# uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
+gunicorn main:app --port 8000 -w 32 -k uvicorn.workers.UvicornWorker
