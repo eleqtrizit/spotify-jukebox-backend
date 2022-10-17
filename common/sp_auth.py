4d3376d4ca1b12028850177a8f7480a6e5430ccd
Agustin Rivera <arivera@arivera-mlt.client.nvidia.com>
2022-10-17 13:52:21 -0700

    callback update

--- common/sp_auth.py
+++ common/sp_auth.py
@@ -3,13 +3,13 @@ from typing import Optional
 
 from spotipy.oauth2 import SpotifyOAuth
 
-CALLBACK = os.environ.get("CALLBACK", "http://localhost:3000/callback")
+REDIRECT_URI = os.environ.get("CALLBACK", "http://localhost:3000/callback")
 
 
 def sp_auth(party_id: Optional[str] = None) -> SpotifyOAuth:
     params = dict(
         scope        = "user-top-read,user-library-read,playlist-modify-public,playlist-modify-private",
-        redirect_uri = "http://localhost:3000/callback",
+        redirect_uri = REDIRECT_URI,
         open_browser = False,
     )
     if party_id:
